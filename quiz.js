aa7d2ef70740298b5476764ddaa0cbcb6993d76e
Mutwingima Mariam <princessmirina45@gmail.com>
2025-10-01 06:47:57 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -8,7 +8,7 @@ function checkAnswer() {
   const submit = document.getElementById("submit-answer");
 
   //compare the user's answer with the correct answer
-  if (userAnswer == correctAnswer) {
+  if (userAnswer === correctAnswer) {
     console.log("Your answer is correct!");
     feedback.textContent = "Correct! Well done.";
   } else {
